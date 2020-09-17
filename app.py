959417f6e646f6437933c32ff4f66f5dd685a285
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-09-16 23:55:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,82 +1,74 @@
-import dash
+import requests 
+from bs4 import BeautifulSoup 
+
+def scrape(link):
+    r = requests.get(link) 
+    
+    soup = BeautifulSoup(r.content, 'html.parser') 
+
+    table = soup.find('div', attrs = {'id':'content'}) 
+    titles=table.findAll('span', attrs = {'class':'calhours-title'})
+    times=table.findAll('span', attrs = {'class':'calhours-times'})
+    
+    return {title.string: time.string.replace('\xa0', ' ') for title, time in zip(titles, times)}
+
+url='https://dining.umich.edu/menus-locations/dining-halls/'
+
+halls=["Bursley","East Quad","Markley","Mosher-Jordan","North Quad","South Quad","Twigs at Oxford"]
+
+links=[url+hall.replace(' ', '-') for hall in halls]
+
+def getTable():
+    headers=[]
+    rows=[scrape(link) for hall, link in zip(halls, links)]
+    
+    for row, hall in zip(rows, halls):
+        headers+=row.keys()
+        row['Hall']=hall
+
+    return set(headers), rows
+
+headers, data=getTable()
+
+from datetime import datetime
+from pytz import timezone
+tz = timezone("America/Detroit")
+now=lambda: datetime.now(tz).strftime("%m/%d/%Y, %H:%M:%S")
+
+from dash import Dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash_table import DataTable
 
-########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-########### Set up the layout
 app.layout = html.Div([
-    html.Label('Max Interest Rate (%): '),
-    dcc.Input(id='rate', type='number'),
-    
-    html.Br(),
-    
-    html.Label('Max Year: '),
-    dcc.Input(id='year', type='number'),
-    
-    dcc.Graph(id='graph'),
-    
+    html.H1('MDining Hours'),
+    html.H3('Updated '+now()),
+
     DataTable(
         id='table',
+        columns=[
+            dict(
+                id=x,
+                name=x,
+            )
+            for x in headers
+        ],
+        data=data,
+
+        sort_action="native",
+        sort_mode="multi",
+        page_action="native",
+	    
+        style_as_list_view=True,
     ),
 ])
-
-@app.callback(
-    [
-        Output(component_id='graph', component_property='figure'),
-        Output(component_id='table', component_property='columns'),
-        Output(component_id='table', component_property='data'),
-    ],
-    [
-        Input(component_id='rate', component_property='value'),
-        Input(component_id='year', component_property='value'),
-    ]
-)
-def update(maxRate, maxYear):
-    rates=[i for i in range(1, maxRate+1)]
-    years=[n for n in range(1, maxYear+1)]
-    data=[]
-    
-    for i in rates:
-        data.append(
-            dict(
-                x=years,
-                y=[round((1-(1+i/100)**-n)/i*100, 4) for n in years],
-                name=f'{i}%'
-            )
-        )
-    
-    matrix=[trace['y'] for trace in data]
-    tposed=[[row[j] for row in matrix] for j in range(len(matrix[0]))]
-    
-    columns=[dict(name=x['name'], id=x['name']) for x in data]
-    sheet=[{column['id']: value for column, value in zip(columns, row)} for i, row in enumerate(tposed)]
-    
-    columns.insert(0, dict(name='Year', id='Year'))
-    for i, row in enumerate(sheet):
-        row['Year']=i+1
-    
-    return (
-        dict(
-            data=data,
-            layout=dict(
-                xaxis=dict(
-                    title='Year'
-                ),
-                yaxis=dict(
-                    title='Present Value'
-                ),
-            )
-        ),
-        columns,
-        sheet
-    )
-
+	
 if __name__ == '__main__':
     app.run_server()
