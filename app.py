9be6a59e6ccd0a6a7b5a340a8bd165d5a8fd8ea1
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-09-16 23:45:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
-import requests 
-from bs4 import BeautifulSoup 
-
-def scrape(link):
-    r = requests.get(link) 
-    
-    soup = BeautifulSoup(r.content, 'html.parser') 
-
-    table = soup.find('div', attrs = {'id':'content'}) 
-    titles=table.findAll('span', attrs = {'class':'calhours-title'})
-    times=table.findAll('span', attrs = {'class':'calhours-times'})
-    
-    return {title.string: time.string.replace('\xa0', ' ') for title, time in zip(titles, times)}
-
-url='https://dining.umich.edu/menus-locations/dining-halls/'
-
-halls=["Bursley","East Quad","Markley","Mosher-Jordan","North Quad","South Quad","Twigs at Oxford"]
-
-links=[url+hall.replace(' ', '-') for hall in halls]
-
-def getTable():
-    headers=[]
-    rows=[scrape(link) for hall, link in zip(halls, links)]
-    
-    for row, hall in zip(rows, halls):
-        headers+=row.keys()
-        row['Hall']=hall
-
-    return set(headers), rows
-
-headers, data=getTable()
-
-from datetime import datetime
-from pytz import timezone
-tz = timezone("America/Detroit")
-now=lambda: datetime.now(tz).strftime("%m/%d/%Y, %H:%M:%S")
+headers, data = ({'Breakfast', 'Dinner', 'Late Night', 'Lunch'},
+ [{'Breakfast': '7:00 am ‑ 10:30 am',
+   'Dinner': '4:30 pm ‑ 8:00 pm',
+   'Hall': 'Bursley',
+   'Late Night': '8:00 pm ‑ 10:00 pm',
+   'Lunch': '10:30 am ‑ 4:30 pm'},
+  {'Breakfast': '7:00 am ‑ 10:30 am',
+   'Dinner': '4:30 pm ‑ 9:00 pm',
+   'Hall': 'East Quad',
+   'Lunch': '10:30 am ‑ 4:30 pm'},
+  {'Breakfast': '7:00 am ‑ 10:30 am',
+   'Dinner': '4:30 pm ‑ 8:00 pm',
+   'Hall': 'Markley',
+   'Lunch': '10:30 am ‑ 2:00 pm'},
+  {'Breakfast': '7:00 am ‑ 10:30 am',
+   'Dinner': '4:30 pm ‑ 9:00 pm',
+   'Hall': 'Mosher-Jordan',
+   'Lunch': '10:30 am ‑ 4:30 pm'},
+  {'Breakfast': '7:00 am ‑ 10:30 am',
+   'Dinner': '4:30 pm ‑ 9:00 pm',
+   'Hall': 'North Quad',
+   'Lunch': '10:30 am ‑ 4:30 pm'},
+  {'Breakfast': '7:00 am ‑ 10:30 am',
+   'Dinner': '4:30 pm ‑ 9:00 pm',
+   'Hall': 'South Quad',
+   'Lunch': '10:30 am ‑ 4:30 pm'},
+  {'Breakfast': '7:00 am ‑ 10:30 am',
+   'Dinner': '4:30 pm ‑ 8:00 pm',
+   'Hall': 'Twigs at Oxford',
+   'Late Night': '8:00 pm ‑ 10:30 pm',
+   'Lunch': '10:30 am ‑ 2:00 pm'}])
 
 from dash import Dash
 import dash_core_components as dcc
