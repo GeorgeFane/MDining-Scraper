81c84008b01a8065111bb85f8cb85a0a5b194a52
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-09-17 09:52:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,60 @@
-import requests 
-from bs4 import BeautifulSoup 
-import html
-
-def scrape(link):
-    r = requests.get(link) 
-    
-    soup = BeautifulSoup(r.content, 'html.parser') 
-
-    table = soup.find('div', attrs = {'id':'content'}) 
-    titles=table.findAll('span', attrs = {'class':'calhours-title'})
-    times=table.findAll('span', attrs = {'class':'calhours-times'})
-    
-    return {title.string: time.string.replace('\xa0', ' ') for title, time in zip(titles, times)}
-
-url='https://dining.umich.edu/menus-locations/dining-halls/'
-
-halls=["Bursley","East Quad","Markley","Mosher-Jordan","North Quad","South Quad","Twigs at Oxford"]
-
-links=[url+hall.replace(' ', '-') for hall in halls]
-
-def getTable():
-    headers=[]
-    rows=[scrape(link) for hall, link in zip(halls, links)]
-    
-    for row, hall in zip(rows, halls):
-        headers+=row.keys()
-        row['Hall']=hall
-
-    return set(headers), rows
-
-headers, data=getTable()
+import requests
+from bs4 import BeautifulSoup
 
 from datetime import datetime
-now=lambda: datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+from pytz import timezone
 
 from dash import Dash
-import dash_core_components as dcc
 import dash_html_components as html
 
-from dash.dependencies import Input, Output, State
 from dash_table import DataTable
+
+
+def scrape(link):
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    table = soup.find('div', attrs={'id': 'content'})
+    titles = table.findAll('span', attrs={'class': 'calhours-title'})
+    times = table.findAll('span', attrs={'class': 'calhours-times'})
+
+    return {
+        title.string: time.string.replace('\xa0', ' ')
+        for title, time in zip(titles, times)
+    }
+
+
+url = 'https://dining.umich.edu/menus-locations/dining-halls/'
+
+halls = [
+    'Bursley',
+    'East Quad',
+    'Markley',
+    'Mosher-Jordan',
+    'North Quad',
+    'South Quad',
+    'Twigs at Oxford'
+]
+
+links = [url+hall.replace(' ', '-') for hall in halls]
+
+
+rows = [scrape(link) for hall, link in zip(halls, links)]
+
+for row, hall in zip(rows, halls):
+    row['Hall'] = hall
+
+headers = [
+    'Hall',
+    'Breakfast',
+    'Brunch',
+    'Lunch',
+    'Dinner',
+    'Late Night'
+]
+
+tz = timezone("America/Detroit")
+stamp = datetime.now(tz).strftime("%m/%d/%Y, %H:%M:%S")
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -48,7 +63,7 @@ server = app.server
 
 app.layout = html.Div([
     html.H1('MDining Hours'),
-    html.H3('Updated '+str(now())),
+    html.H3('Last updated: ' + stamp),
 
     DataTable(
         id='table',
@@ -59,15 +74,15 @@ app.layout = html.Div([
             )
             for x in headers
         ],
-        data=data,
+        data=rows,
 
         sort_action="native",
         sort_mode="multi",
         page_action="native",
-	    
+
         style_as_list_view=True,
     ),
 ])
-	
+
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(port='8080')
