fd082c80266bdfaf1842c657ce27fbac44830f27
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-09-24 20:05:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
-
 from datetime import datetime
 from pytz import timezone
 
 from dash import Dash
 import dash_html_components as html
-
 from dash_table import DataTable
 
-# for a dining hall's MDining link,
-# this gets all meals and hours that day
-def scrape(link):
-    r = requests.get(link)
-    soup = BeautifulSoup(r.content, 'html.parser')
+url = 'https://dining.umich.edu/menus-locations/dining-halls/'
+halls = ["Bursley", "East Quad", "Markley", "Mosher-Jordan", "North Quad", "South Quad", "Twigs at Oxford"]
 
-    table = soup.find('div', attrs={'id': 'content'})
-    titles = table.findAll('span', attrs={'class': 'calhours-title'})
-    times = table.findAll('span', attrs={'class': 'calhours-times'})
+tz = timezone("America/Detroit")
+now = lambda: datetime.now(tz)
+ptime = lambda time, form: datetime.strptime(time, form).time()
+ftime = lambda string, form: datetime.strftime(string, form)
+
+def menu(hall):
+    link = url + hall.replace(' ', '-')
+
+    # stores entire page's HTML in soup
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, 'html5lib')
+
+    # gets mealtimes as time objects
+    spans = soup.findAll('span', attrs = {'class':'calhours-times'})
+    pairs = [span.string.replace('\xa0', ' ').split(' ‑ ') for span in spans]
+    times = [[ptime(x, '%I:%M %p')for x in pair] for pair in pairs]
+
+    # fallback returns
+    foods = 'Closed'
+    hours = 'Closed'
+
+    # gets divs for all meals
+    divs = soup.find_all('div', attrs = {'class': 'courses'})
+
+    # checks if any meal is open
+    boos = [start <= now().time() <= end for start, end in times]
+
+    # gets foods for open meal
+    if any(boos):
+        ind = boos.index(True)
+        hours = pairs[ind][1]
+    
+        meal = divs[ind].find_all('div', attrs = {'class': 'item-name'})
+        foods = ', '.join([food.getText().strip() for food in meal])
 
     return {
-        title.string: time.string.replace('\xa0', ' ')
-        for title, time in zip(titles, times)
+        'Hall': hall,
+        'Closing': hours,
+        'Foods': foods,
     }
 
-# dining hall links only differ after the rightmost /
-# below is the part that all links share
-url = 'https://dining.umich.edu/menus-locations/dining-halls/'
-
-halls = [
-    'Bursley',
-    'East Quad',
-    'Markley',
-    'Mosher-Jordan',
-    'North Quad',
-    'South Quad',
-    'Twigs at Oxford'
-]
-
-# constructs links for all dining halls
-links = [url+hall.replace(' ', '-') for hall in halls]
-
-# gets meals and hours for all dining halls
-rows = [scrape(link) for link in links]
-
-# adds hall name to each hall's dictionary
-for row, hall in zip(rows, halls):
-    row['Hall'] = hall
-
-headers = [
-    'Hall',
-    'Breakfast',
-    'Brunch',
-    'Lunch',
-    'Dinner',
-    'Late Night'
-]
-
-#stores current time in EST
-tz = timezone("America/Detroit")
-stamp = datetime.now(tz).strftime("%m/%d/%Y, %H:%M:%S")
+headers = ['Hall', 'Closing', 'Foods']
+data = [menu(hall) for hall in halls]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -67,8 +60,8 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    html.H1('MDining Hours'),
-    html.H3('Last updated: ' + stamp),
+    html.H1('MDining Foods'),
+    html.H3('Updated ' + ftime(now(), '%I:%M %p')),
 
     DataTable(
         id='table',
@@ -79,15 +72,10 @@ app.layout = html.Div([
             )
             for x in headers
         ],
-        data=rows,
-
-        sort_action="native",
-        sort_mode="multi",
-        page_action="native",
-
-        style_as_list_view=True,
+        data = data,
+        style_cell = dict(textAlign = 'left'),
     ),
 ])
 
 if __name__ == '__main__':
-    app.run_server(port='8080')
+    app.run_server()
